b7aa7fa7196b18cacc2eb7cf2f1dc425d79e8952
Roel Hogervorst <hogervorst.rm@gmail.com>
2016-08-13 20:35:31 +0200

    text added about plotting includes reference and empty plot

--- server.R
+++ server.R
@@ -7,17 +7,29 @@
 # alle functies en input van ui
 
 library(shiny)
-library(heisertransform)
+library(ggplot2)
+source("R/heisertransformfunctions.R")
+
 shinyServer(function(input, output) {
+    output$text1 <- renderText({ 
+       paste(1-input$leftcorner -input$topcorner)
+    })
+    #currentprop <- sum(input$leftcorner, input$topcorner, input$rightcorner)
+    output$text2 <- renderText({
+        paste("The proportions are now left:", 
+              input$leftcorner,"and top;", 
+              input$topcorner, 
+              "and right;",
+              (1-input$leftcorner -input$topcorner))
+    })
 
   output$distPlot <- renderPlot({
-
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+      triangles <- vertices_create(input$leftcorner, 
+                                   input$topcorner,
+                                   (1-input$leftcorner -input$topcorner))
+      names(triangles) <- c("x", "y")
+      g<-ggplot() +lines(triangles, x,y,"blue" )+ corners(triangles, x,y, size = 5) +theme_heiser
+    print(g)
 
   })
 
--- ui.R
+++ ui.R
@@ -10,21 +10,43 @@ library(shiny)
 shinyUI(fluidPage(
 
   # Application title
-  titlePanel("plots"),
+  titlePanel("Transformation player"),
+  
 
-  # Sidebar with a slider input for number of bins
+  # Sidebar with a slider input 
   sidebarLayout(
     sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
+        h2("Basics behind Heiser 2004"),
+    p("If we visualize the three proportions of a dataset as a distance
+    on a three axis (x,y,z) then we can connect the three points
+    inside the cube with lines. This intersection is a triangle as displayed here.
+    If the proportions change then the shape of the triangle changes as well."),
+  h3("Instructions"),
+  p("use the two sliders to change the proportions, the third one will change
+    automatically. Since proportions of a group can never exceed 1, the third
+    proportion will be calculated automagically. Larger proportions pull the
+    triangle towards them."),
+        sliderInput("leftcorner",
+                  "Proportion 1 (left):",
+                  min = 0,
+                  max = 1,
+                  value = .3),
+        sliderInput("topcorner",
+                  "Proportion 2 (top):",
+                  min = 0,
+                  max = 1,
+                  value = .3),
+        
+        textOutput("text2")
+),
 
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("distPlot")
+      plotOutput("distPlot"),
+      p("Find more information in: Heiser, W. J. (2004). 
+Geometric representation of association between categories. 
+Psychometrika, 69(4), 513–545. http://doi.org/10.1007/bf02289854
+ ")
     )
   )
 ))
